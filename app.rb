bfb3300e6b5c5a8804049f2a69c1bed88fbb248b
Anna Sherman <sherman.anna@gmail.com>
2015-11-19 15:05:07 -0600

    do the thing

--- app.rb
+++ app.rb
@@ -2,7 +2,15 @@ require 'bundler'
 Bundler.require
 
 get '/' do
+  erb :hello
+end
+
+get '/world' do
+  erb :world
+end
+
+get '/mom' do
   {
-    :message => 'This is a boilerplate'
+    :message => 'Your momma so fat'
   }.to_json
 end
---
+++ public/app.js
@@ -0,0 +1,6 @@
+window.onload = function (){
+
+document.body.innerHTML = "where the hood at"
+
+  console.log('ready');
+}
